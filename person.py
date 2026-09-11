881d811814b13bac9cfc814f6462d2ae71b44315
anushaannigeri51-spec <anushaannigeri51@gmail.com>
2026-09-11 10:12:09 +0530

    Update and rename README.md to person.py

---
+++ person.py
@@ -0,0 +1,11 @@
+# person
+first name = inpute("enter a first name")
+last name = inpute("enter a last name")
+division = input("enter a divison")
+college = inpute("enter a college")
+
+print("presonal details")
+print("first name:",first name)
+print("last name:",last name)
+print("divison:",divison)
+print("college:",college)
